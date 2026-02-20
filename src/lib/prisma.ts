6cd122475b41cad19009bd8967b2fbc1b587d801
dhananjoycd <dcdmf2020@gmail.com>
2026-02-20 23:55:08 +0600

    backend deploy done

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,10 +2,9 @@ import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../../generated/prisma/client.js";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`; //neondb
 
 const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
-
